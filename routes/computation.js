1b4746b1f0180c4a7d88fbb0ceb1f49c40d039a5
Devabhaktineni Sayendranadh Chowdary <devabhaktinenisayendra@gmail.com>
2023-10-26 00:12:06 -0500

    added

--- routes/computation.js
+++ routes/computation.js
@@ -1,32 +1,21 @@
-const express = require('express');
-const app = express();
-const port = 3000; // Set your desired port
+var express = require('express');
+var router = express.Router();
 
-app.get('/computation', (req, res) => {
+// Define the "computation" route
+router.get('/', function(req, res, next) {
+  
+  var x = parseFloat(req.query.x) || (Math.random() * 10); 
+  var angleRadians = calcAngle(x, 10); // Assuming a hypotenuse of 10
 
-  const id = 7;
-  const lastDigit = id % 10;
+  // Format the response string
+  var responseString = `Math.acos applied to ${x} is ${angleRadians}`;
 
-  const mathFunctions = {
-    0: Math.acosh,
-    1: Math.log1p,
-    2: Math.tan,
-    3: Math.sqrt
-  };
-
-  const x = req.query.x ? parseFloat(req.query.x) : Math.random() * 10;
-
-  const selectedFunction = mathFunctions[lastDigit];
-
-  if (selectedFunction) {
-    const result = selectedFunction(x);
-    const response = `${selectedFunction.name} applied to ${x} is ${result.toFixed(2)}`;
-    res.json({ result: response });
-  } else {
-    res.status(400).json({ error: "Invalid last digit, no corresponding math operation." });
-  }
+  // Send the response
+  res.send(responseString);
 });
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+// Helper function to calculate angle using Math.acos
+function calcAngle(adjacent, hypotenuse) {
+  return Math.acos(adjacent / hypotenuse);
+}
+module.exports=router;
